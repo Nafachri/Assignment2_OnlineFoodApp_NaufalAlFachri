3b3f37e5df9d4280a5b9d51dbb0dbe87640bee2e
Nafachri <nafa.create@gmail.com>
2024-07-08 09:57:14 +0700

    Initial Commit

---
+++ OnlineFoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineFoodApp
+//
+//  Created by Naufal Al-Fachri on 08/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
